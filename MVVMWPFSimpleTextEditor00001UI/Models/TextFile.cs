2ec47eac7ef967d0c33b23d25d4d30ff7a2e1611
Brandon Weaver <a7xncob@gmail.com>
2020-05-06 15:17:40 -0400

    Add new, open and save commands.

--- MVVMWPFSimpleTextEditor00001UI/Models/TextFile.cs
+++ MVVMWPFSimpleTextEditor00001UI/Models/TextFile.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,5 +12,23 @@ namespace MVVMWPFSimpleTextEditor00001UI.Models
     {
         public string Path { get; set; }
         public string Text { get; set; }
+
+        public void Save()
+        {
+            if (this.Path != null)
+                File.WriteAllText(this.Path, this.Text);
+            else
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog
+                {
+                    Filter = "Text (*.txt)|*.txt"
+                };
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    this.Path = saveFileDialog.FileName;
+                    File.WriteAllText(saveFileDialog.FileName, this.Text);
+                }
+            }
+        }
     }
 }
--- MVVMWPFSimpleTextEditor00001UI/ViewModels/TextFileViewModel.cs
+++ MVVMWPFSimpleTextEditor00001UI/ViewModels/TextFileViewModel.cs
@@ -1,7 +1,9 @@
-﻿using MVVMWPFSimpleTextEditor00001UI.Models;
+﻿using Microsoft.Win32;
+using MVVMWPFSimpleTextEditor00001UI.Models;
 using MVVMWPFSimpleTextEditor00001UI.ViewModels.Commands;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,13 +24,45 @@ namespace MVVMWPFSimpleTextEditor00001UI.ViewModels
             }
         }
 
+        public VoidCommand NewCommand { get; set; }
+        public VoidCommand OpenCommand { get; set; }
+        public VoidCommand SaveCommand { get; set; }
         public VoidCommand ExitCommand { get; set; }
 
         public TextFileViewModel()
         {
             this.TextFile = new TextFile();
 
+            this.NewCommand = new VoidCommand(New);
+            this.OpenCommand = new VoidCommand(Open);
+            this.SaveCommand = new VoidCommand(Save);
             this.ExitCommand = new VoidCommand(Exit);
+        }
+
+        private void New()
+        {
+            this.TextFile = new TextFile();
+        }
+
+        private void Open()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Text (*.txt)|*.txt"
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                this.TextFile = new TextFile
+                {
+                    Path = openFileDialog.FileName,
+                    Text = File.ReadAllText(openFileDialog.FileName)
+                };
+            }
+        }
+
+        private void Save()
+        {
+            this.TextFile.Save();
         }
 
         private void Exit()
